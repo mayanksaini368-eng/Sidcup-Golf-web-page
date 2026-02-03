c9bc816e48f886159d46e2fe1da78c21d541435d
mayanksaini368-eng <mayanksaini368@gmail.com>
2026-02-03 23:52:35 +0530

    saving my changes

--- First website/script.js
+++ First website/script.js
@@ -36,7 +36,6 @@ gsap.to("#nav",{
     scrollTrigger:{
      trigger:"#nav",
      scroller:"body",
-     markers:true,
      start:"top -10%",  
      end:"top -11%",
      scrub: 1
@@ -48,7 +47,7 @@ gsap.to("#main",{
     scrollTrigger:{
         trigger:"#main",
         scroller:"body",
-        markers:true,
+        
         start:"top -25%",
         end:"top -70%",
         scrub:2
